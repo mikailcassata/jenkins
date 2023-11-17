b33c8febc11f235f9aa9d4a96175b20e5da802e6
mikailcassata <71608717+mikailcassata@users.noreply.github.com>
2023-11-17 19:39:27 +0000

    Update provider.tf

--- t2/provider.tf
+++ t2/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 provider "docker" {
   #host     = "unix:///var/run/docker.sock"
-  host     = "ssh://root@10.0.2.6"
+  host     = "ssh://root@10.0.2.4"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
 
 
